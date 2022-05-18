b0a43f455d6286832a3dbe4c7167213e301b4590
dominikmartinez <38652544+dominikmartinez@users.noreply.github.com>
2022-05-18 13:16:27 +0000

    add scripts needed for training, changes in structure of training cfg, add train.sh

---
+++ shell_scripts/train.sh
@@ -0,0 +1,11 @@
+working_dir=/home/domi_zenitram/ABCD-ACL2021
+python_dir=$working_dir/python_scripts
+data_dir=$working_dir/data/MinWiki
+venv_dir=/home/domi_zenitram/venvs/venv_abcd
+
+source $venv_dir/bin/activate
+
+python -u $python_dir/main.py
+
+
+
