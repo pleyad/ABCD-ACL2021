25a435645ddac0c99006b241b5dfe13a1bfa260b
Niclas Bodenmann <niclaslinus.bodenmann@uzh.ch>
2022-05-29 22:36:20 +0200

    Moved acl checkpoints from main to clean and furthered glove setup (not finished)

--- shell_scripts/glove_setup.sh
+++ shell_scripts/glove_setup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-curl -oL https://huggingface.co/stanfordnlp/glove/resolve/main/glove.6B.zip
+echo "ABCDre Info: Checking for directory for glove 'data/glova'."
+if [[ -d "data/glove/" ]]
+then
+    echo "ABCDre Info: Directory 'data/glove/' already exists."
+else
+    echo "ABCDre Info: Creating directory for CoreNLP 'data/glove/' ..."
+    mkdir data/glove
+fi
+cd data/glove
+
+curl -LJO https://huggingface.co/stanfordnlp/glove/resolve/main/glove.6B.zip
+unzip glove.6B.zip
+
+cd ../..
